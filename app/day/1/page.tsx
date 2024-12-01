3031426ef1d7d7651f7cc7d88c094574e4db7a0e
Timothy Andrew <mail@timothyandrew.net>
2024-12-01 15:54:22 +0000

    layout

--- app/day/1/page.tsx
+++ app/day/1/page.tsx
@@ -55,16 +55,16 @@ export default async function Home() {
 
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div>
+    <ul>
+      <li>
         Sample: {solvePart1(parseInput(sample))}
-      </div>
-      <div>
+      </li>
+      <li>
         Part 1: {solvePart1(parseInput(input))}
-      </div>
-      <div>
+      </li>
+      <li>
         Part 2: {solvePart2(parseInput(input))}
-      </div>
-    </div>
+      </li>
+    </ul>
   )
 }
